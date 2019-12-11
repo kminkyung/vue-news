4a97ed87a9e2be32c5e8ea4144b2606cbf9c237c
Minkyung KIM <minkyungkim@MacBook-Pro-de-Minkyung.local>
2019-12-11 20:24:11 +0900

    routing, toolbar created

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router'; 
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+	routes : [
+		{
+			path : '/',
+			redirect : '/news'
+		},
+		{
+			// path : url 주소
+			path : '/news',
+			// component : url로 갔을 때 보여줄 component
+			component : NewsView
+		},
+		{
+			path : '/ask',
+			component : AskView
+		},
+		{
+			path : '/jobs',
+			component : JobsView
+		}
+	]
+})
